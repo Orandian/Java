793ba6891d4b017dd9fb2617f7cce8bbe0d983a9
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2023-11-05 21:59:29 +0630

    still loading

---
+++ LotteryNumberGenerator/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int min = 1, max = 49, slots = 6;
+
+        int[] lotteryArray = new int[slots];
+        boolean isRepeated;
+        int randomNumber = 0;
+
+        for (int indexDrawn = 0; indexDrawn < slots; indexDrawn++) {
+            do {
+                isRepeated = false;
+                // 1 to 49
+                randomNumber = rand.nextInt(max + 1 - min) + min;
+                // check for repeated number against the filled non-zero elements
+                for (int k = 0; k <= indexDrawn; k++) {
+                    // check against each filled non-zero elements hope that none repeats the random number
+                    if (lotteryArray[k] == randomNumber){
+                        isRepeated = true;
+                        break;
+                    }
+                }
+            } while (isRepeated);
+            lotteryArray[indexDrawn] = randomNumber;
+        }
+
+        Arrays.sort(lotteryArray);
+        System.out.println("The result fo the lottery: ");
+
+        for (int i = 0; i < slots; i++){
+            System.out.print(lotteryArray[i] + " ");
+        }
+    }
+}
